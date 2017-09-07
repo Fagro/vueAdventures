8f98515cd6a7b11f54f4a9dd268c269cac722a5a
fagro <fagro.vizcaino@gmail.com>
2017-09-07 17:37:49 -0400

    introduction to components

---
+++ vueCli/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <p>Server status : {{ status }}</p>
+    <hr>
+    <button @click="changeStatus()">change status</button>
+</div>
+
+</template>
+<<script>
+export default {
+    data : function(){
+        return {
+            status : 'critical status'
+        }
+    },
+    methods: {
+        changeStatus(){
+            this.status = 'Normal'
+        }
+    }
+}
+</script>
+
