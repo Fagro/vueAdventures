1c6f0b7a0a2da4d4f78ff1d127fc26dd768386f0
fagro <fagro.vizcaino@gmail.com>
2017-09-16 11:50:35 -0400

    component and templates

---
+++ component-exercise-7/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <header-app></header-app>
+    <hr>
+      <server-index></server-index>
+    <hr>
+    <footer-app></footer-app>
+  </div>
+</template>
+
+<script>
+
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import ServerIndex from './ServerIndex.vue'
+
+export default {
+  components: {
+    'header-app': Header,
+    'footer-app': Footer,
+    'server-index': ServerIndex
+  }
+}
+</script>
+
+<style>
+
+</style>
